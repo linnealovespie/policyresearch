dd03f709c61c7199c084c960f728d04950f24bf3
Linnea May <linneamay@pop-os.localdomain>
2020-06-07 16:59:56 -0400

    basic pipeline of finding a tweet and its responses

---
+++ twitterConversations/twurltest.sh
@@ -0,0 +1,3 @@
+twurl "/1.1/tweets/search/fullarchive/ConversationResearch.json" -A "Content-Type: application/json" -d '{"query":"to:HillaryClinton lang:en","maxResults":"100","fromDate":"201802280000","toDate":"201802282359"}' 
+
+
